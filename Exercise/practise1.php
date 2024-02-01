9ba32c2ab05aba80dd5dbec8baad177179c3ce14
Nirmal Rathod <142677259+nirmalrathod23@users.noreply.github.com>
2024-02-01 14:32:14 +0000

    array programs

--- Exercise/practise1.php
+++ Exercise/practise1.php
@@ -15,5 +15,11 @@ $names = array("nirmal","krishna","mansi","hardik","yasmin",23);
 
     echo '<br>' . sizeof($names);
 
+    $names = array("NirmalKumar","Devendrabhai","Rathod",22);
+
+    echo "<br><h4>First Name:{$names[0]}</h4>";
+    echo "<h4>Middle Name:{$names[1]}</h4>";
+    echo "<h4>Last Name:{$names[2]}</h4>";
+    echo "<h4>Age:{$names[3]}</h4><br>";
 
 ?>
